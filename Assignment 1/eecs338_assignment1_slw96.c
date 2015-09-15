bf4a9935c2044f01b04464a26f85737bfc12e9fa
SWhelan <slw96@case.edu>
2015-09-15 01:04:01 -0500

    Worst comes to worse use this one.
    Prints everything necessary loops take time on this computer but do not take
    up enough time on the lab computers. Needs comments. A little cleanup and
    clarifying questions of instructions. Double check mans for possible errors.
    The timming is all correct for this one.

--- Assignment 1/eecs338_assignment1_slw96.c
+++ Assignment 1/eecs338_assignment1_slw96.c
@@ -12,14 +12,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <errno.h>
+
+#define LOOP_ITERATIONS 0x7fffffff
 
 int main() {
     int i;
 	pid_t child_fpid;	
 	int termination_status;
-	pid_t child_id;
-	printf("Hello, I am the parent process. My process id is %d. Let's get started!\n", getpid());
-    
+	pid_t child_id; 
+	printf("I am the parent process. My process id is %d.\n", getpid());
+    print_ids();
     for(i = 1; i < 5; i++){
         child_fpid = fork();
     
@@ -27,6 +33,8 @@ int main() {
 	        perror("An error occured while executing fork.");
 	        return EXIT_FAILURE;
         } else if (child_fpid == 0){
+            print_ids();
+            sleep(3);
             if(i == 1){
                 return child1();
             } else if(i == 2){
@@ -40,53 +48,98 @@ int main() {
     }
 	
 	
-	sleep(15);
+	sleep(30);
 	while((child_id = wait(&termination_status)) > 0){
-    	printf("I the parent class lovingly waited for my child (with process id %d). They terminated with an exit status of %d.\n", child_id, termination_status);
+    	printf("A child process with id %d terminated with an exit status of %d.\n", child_id, termination_status);
     }
+	
+	loop_and_print_times();
 	
     return EXIT_SUCCESS;
 
-}
+}	
 
 int child1(){
     //this is the first child process
-    printf("Hello, I am the first child! My process id is %d\n", getpid());
     printf("(n (n-2)) bionomial coefficient computations of integers n=2, 3, 10, start now!\n");
+    loop_and_print_times();
     return EXIT_SUCCESS;
 }
 
 int child2(){
-    int i;
-    sleep(1);
-    for(i = 2; i < 11; i++){
-        if(i % 2 == 0){
-            printf("Child 2 process reporting in with %d choose %d: %d.\n", i, i-2, binomial_coefficient(i, i-2));
-        } else {
-            sleep(1);
-        }
-    }
-    return EXIT_SUCCESS;
+    sleep(2);
+    binomial_loop(2);
 }
 
 int child3(){
-    int i;
-    for(i = 2; i < 11; i++){
-        if(i % 2 != 0){
-            printf("Child 3 process reporting in with %d choose %d: %d.\n", i, i-2, binomial_coefficient(i, i-2));
-        } else {
-            sleep(1);
-        } 
-    }
-    return EXIT_SUCCESS;
+    sleep(3);
+    binomial_loop(3);
 }
 
 int child4(){
-    sleep(10);
-    printf("Child 4 process reporting with the results of ls -l.\n");
+    sleep(15);
+    printf("Process (pid %d) will loop for a while and print how long it has taken then will run ls -l.\n", getpid());
+    loop_and_print_times();
+    printf("Process (pid %d) executing ls -l:\n", getpid());
     execlp("ls", "ls", "-l", (char *) NULL);
     perror("Error occured while calling exec on ls -l.");
     return EXIT_FAILURE;
+}
+
+int print_ids(){
+    char * username;    
+    username = (char *) cuserid((int*) NULL);
+    if(username == NULL){
+        perror("There was an error getting the username.");
+        return EXIT_FAILURE;
+    }
+    printf("The current process id: %d\n", getpid());
+    printf("The username: %s\n", username);
+    printf("The real user id: %d\n", getuid());
+    printf("The effective user id: %d\n", geteuid());
+    printf("The group id: %d\n", getgid());
+    print_seperator();
+    return EXIT_SUCCESS;
+}
+
+int loop_and_print_times(){
+    int i;
+    time_t current_time;
+    struct rusage usage;
+    i = 0;
+    while(i < 10000){
+        i++;
+    }
+    
+    if(getrusage(RUSAGE_SELF, &usage) < 0){
+        perror("There was an error getting the system and user times for process.");
+        return errno;
+    }
+    
+    time(&current_time);
+    printf("The current process with pid %d is about to terminate.\n", getpid());
+    printf("The current time is: %s", ctime(&current_time));
+    printf("The user CPU time is: %d\n", (int)usage.ru_utime.tv_usec);
+    printf("The system CPU time is: %d\n", (int)usage.ru_stime.tv_usec);
+    print_seperator();
+    return EXIT_SUCCESS;
+}
+
+int binomial_loop(int start){
+    int i;
+    for(i = start; i < 11; i = i + 2){
+        printf("Process (pid %d) calculated binomial coefficient %d with n = %d and k = %d.\n", getpid(), binomial_coefficient(i, i-2), i, i-2);
+        sleep(2);
+    }
+    if(start == 2){
+        printf("Finished finding binomial coefficients.\n");
+        print_seperator();
+    }
+    if(start == 3){
+        sleep(2);
+    }
+    loop_and_print_times();
+    return EXIT_SUCCESS;
 }
 
 //Get the binomial coefficient - equivalent to n choose k
@@ -101,4 +154,8 @@ int factorial(int n){
     } else {
         return n * factorial(n-1);
     }
+}
+
+int print_seperator(){
+    printf("======================================\n");
 }
